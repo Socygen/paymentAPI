54ebddb4e9f5090ccedbee83e878e75912c16ce9
SocyGen EduTech Private Limited <115183627+Socygen@users.noreply.github.com>
2024-12-22 16:32:01 +0530

    Update stateModel.js

--- models/stateModel.js
+++ models/stateModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const StateSchema = new mongoose.Schema({
     stateName: String,
-},{timestamps : true});
+}, { timestamps: true });
 
 module.exports = mongoose.model('State', StateSchema);
-module.exports = StateSchema;
